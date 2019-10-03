99501888e8c5f35b8ae91141273c44885887f918
Wacker <RobertWacker>
2019-10-03 17:04:39 +0700

    Final commit fix

--- server/routes/debt.route.js
+++ server/routes/debt.route.js
@@ -25,7 +25,6 @@ router.get(BASE_URL, async (ctx) => {
 router.get(BASE_URL + "/nopayment", async (ctx) => {
  try {
    const result = await request.getDebtNopayment();
-   console.log(result);
    ctx.status = 200;
    ctx.body = {
      data: result,
